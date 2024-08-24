494298a1af453640ed36066ce91d0e0d2b1259a1
Davi Matos Marques Silva <95830402+vdesgo@users.noreply.github.com>
2024-08-24 11:36:59 -0300

    update 2048.js

--- 2048.js
+++ 2048.js
@@ -8,13 +8,7 @@ window.onload = function() {
 }
 
 function setGame() {
-    // board = [
-    //     [2, 2, 2, 2],
-    //     [2, 2, 2, 2],
-    //     [4, 4, 8, 8],
-    //     [4, 4, 8, 8]
-    // ];
-
+    
     board = [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
@@ -31,7 +25,7 @@ function setGame() {
             document.getElementById("board").append(tile);
         }
     }
-    //create 2 to begin the game
+
     setTwo();
     setTwo();
 
@@ -39,7 +33,7 @@ function setGame() {
 
 function updateTile(tile, num) {
     tile.innerText = "";
-    tile.classList.value = ""; //clear the classList
+    tile.classList.value = ""; 
     tile.classList.add("tile");
     if (num > 0) {
         tile.innerText = num.toString();
@@ -73,24 +67,23 @@ document.addEventListener('keyup', (e) => {
 })
 
 function filterZero(row){
-    return row.filter(num => num != 0); //create new array of all nums != 0
+    return row.filter(num => num != 0); 
 }
 
 function slide(row) {
-    //[0, 2, 2, 2] 
-    row = filterZero(row); //[2, 2, 2]
+
+    row = filterZero(row); 
     for (let i = 0; i < row.length-1; i++){
         if (row[i] == row[i+1]) {
             row[i] *= 2;
             row[i+1] = 0;
             score += row[i];
         }
-    } //[4, 0, 2]
-    row = filterZero(row); //[4, 2]
-    //add zeroes
+    } 
+    row = filterZero(row); 
     while (row.length < columns) {
         row.push(0);
-    } //[4, 2, 0, 0]
+    }
     return row;
 }
 
@@ -109,10 +102,10 @@ function slideLeft() {
 
 function slideRight() {
     for (let r = 0; r < rows; r++) {
-        let row = board[r];         //[0, 2, 2, 2]
-        row.reverse();              //[2, 2, 2, 0]
-        row = slide(row)            //[4, 2, 0, 0]
-        board[r] = row.reverse();   //[0, 0, 2, 4];
+        let row = board[r];         
+        row.reverse();              
+        row = slide(row)            
+        board[r] = row.reverse();  
         for (let c = 0; c < columns; c++){
             let tile = document.getElementById(r.toString() + "-" + c.toString());
             let num = board[r][c];
@@ -125,10 +118,7 @@ function slideUp() {
     for (let c = 0; c < columns; c++) {
         let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
         row = slide(row);
-        // board[0][c] = row[0];
-        // board[1][c] = row[1];
-        // board[2][c] = row[2];
-        // board[3][c] = row[3];
+       
         for (let r = 0; r < rows; r++){
             board[r][c] = row[r];
             let tile = document.getElementById(r.toString() + "-" + c.toString());
@@ -144,10 +134,6 @@ function slideDown() {
         row.reverse();
         row = slide(row);
         row.reverse();
-        // board[0][c] = row[0];
-        // board[1][c] = row[1];
-        // board[2][c] = row[2];
-        // board[3][c] = row[3];
         for (let r = 0; r < rows; r++){
             board[r][c] = row[r];
             let tile = document.getElementById(r.toString() + "-" + c.toString());
@@ -163,7 +149,7 @@ function setTwo() {
     }
     let found = false;
     while (!found) {
-        //find random row and column to place a 2 in
+       
         let r = Math.floor(Math.random() * rows);
         let c = Math.floor(Math.random() * columns);
         if (board[r][c] == 0) {
@@ -180,7 +166,7 @@ function hasEmptyTile() {
     let count = 0;
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
-            if (board[r][c] == 0) { //at least one zero in the board
+            if (board[r][c] == 0) { 
                 return true;
             }
         }
